38ccd8d830c095d662131519f846a1aba63e0767
Vestimir Markov <me@vestimir.com>
2020-08-06 19:27:31 +0300

    Remove form input importants

--- src/Input.js
+++ src/Input.js
@@ -3,20 +3,20 @@ import reset from './reset'
 
 export default styled.input`
   ${reset};
-  border: 0 !important;
-  outline: 0 !important;
-  border-radius: 4px !important;
-  border: 0 !important;
-  width: 100% !important;
-  padding: 0.5em 0.75em !important;
-  border: 1px solid ${(props) => props.theme.colors.gray} !important;
-  background-color: ${(props) => props.theme.colors.trueWhite} !important;
+  border: 0;
+  outline: 0;
+  border-radius: 4px;
+  border: 0;
+  width: 100%;
+  padding: 0.5em 0.75em;
+  border: 1px solid ${(props) => props.theme.colors.gray};
+  background-color: ${(props) => props.theme.colors.trueWhite};
 
   &:hover {
-    border-color: ${(props) => props.theme.colors.darkGray} !important;
+    border-color: ${(props) => props.theme.colors.darkGray};
   }
 
   &:focus {
-    border-color: ${(props) => props.theme.colors.primary} !important;
+    border-color: ${(props) => props.theme.colors.primary};
   }
 `
--- src/reset.js
+++ src/reset.js
@@ -2,12 +2,12 @@ import { css } from 'styled-components'
 import theme from './theme'
 
 const reset = css`
-  box-sizing: border-box !important;
-  color: ${theme.colors.black} !important;
+  box-sizing: border-box;
+  color: ${theme.colors.black};
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-    Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif !important;
-  font-size: ${theme.font.base} !important;
-  line-height: ${theme.font.line} !important;
+    Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  font-size: ${theme.font.base};
+  line-height: ${theme.font.line};
 `
 
 export default reset
