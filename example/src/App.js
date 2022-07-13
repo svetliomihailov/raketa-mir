1a76265b42ef2fb351fc7ee6dbb3204200a11646
Svetlozar Mihailov <svetliomihailov@gmail.com>
2022-07-13 12:54:03 +0300

    Add language switcher to the available elements

--- example/src/App.js
+++ example/src/App.js
@@ -24,6 +24,7 @@ import {
   Navigation,
   NavItem,
   DataGrid,
+  LanguageSwitcher,
 } from '@raketa-cms/raketa-mir'
 
 const App = () => {
@@ -173,6 +174,16 @@ const App = () => {
                     src='data:image/svg+xml;base64,PHN2ZyBzdHJva2U9ImN1cnJlbnRDb2xvciIgZmlsbD0iY3VycmVudENvbG9yIiBzdHJva2Utd2lkdGg9IjAiIHZpZXdCb3g9IjAgMCA1MTIgNTEyIiBoZWlnaHQ9IjFlbSIgd2lkdGg9IjFlbSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj48cGF0aCBkPSJNMjc3LjMgMzUuMTFsLTMyLjEgMi4xMi0xIDMyLjkzIDMzLjYuMDUtLjUtMzUuMXpNMTUxLjcgODguMDRMNjcuOTYgMTE4LjkgMTUyIDE2OS4ybDE4OC02LjIgMi40LTc0LjcyLTE5MC43LS4yNHpNMjc5LjIgMTgzbC0zOC4zIDEuMy0uOCAyNy44IDM5LjUgMS4yLS40LTMwLjN6bS05My4xIDQ1LjZsLjggNjQuMiAyMDAuNC45IDUxLjctMjkuMi01NS44LTMwLjItMTk3LjEtNS43em01MS4xIDgyLjVsLTUgMTc1LjMgNTAuOS42LTIuMy0xNzUuNy00My42LS4yeiI+PC9wYXRoPjwvc3ZnPgo='
                   />
                 </a>
+              </NavItem>
+
+              <NavItem active>
+                <LanguageSwitcher
+                  current='EN'
+                  navigation={[
+                    { path: '/bg/', label: 'BG' },
+                    { path: '/de/', label: 'DE', newItem: true },
+                  ]}
+                />
               </NavItem>
             </Navigation>
           </div>
